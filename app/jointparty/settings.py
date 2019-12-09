2a76411801bfdfaa4ce29c2724437012ab48c52a
kooo0401 <56294747+kooo0401@users.noreply.github.com>
2019-12-09 19:02:02 +0900

    test

--- app/jointparty/settings.py
+++ app/jointparty/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '=llv2rzj$^vk)wx7n_u9v$o(ic2i-7w)m&0il3vlamiei2#&d6'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '=llv2rzj$^vk)wx7n_u9v$o(ic2i-7w)m&0il3vlamiei2#&d6'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
