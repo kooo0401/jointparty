7aee1fcce7bdcf39e2800fbd8f4394e552b1095d
kooo0401 <56294747+kooo0401@users.noreply.github.com>
2019-12-15 20:16:45 +0900

    signup_create

--- app/match/urls.py
+++ app/match/urls.py
@@ -5,5 +5,8 @@ app_name = 'match'
 
 urlpatterns = [
     path('', index.top, name='index'),
+    path('login/', index.Login.as_view(), name='login'),
+    path('logout/', index.Logout.as_view(), name='logout'),
+    path('users/', user.gets, name='users'),
     path('users/signup/', user.signup, name='signup'),
 ]
