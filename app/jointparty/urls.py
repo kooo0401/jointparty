c7f8fb6ea1f9c7e659fe5cc069db2f075efa2c89
kooo0401 <56294747+kooo0401@users.noreply.github.com>
2019-12-18 11:50:46 +0900

    debug_create

--- app/jointparty/urls.py
+++ app/jointparty/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
+# デバッグツール（django-debug-toolbar）の設定
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('match.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += (
+        url('match.urls', include(debug_toolbar.urls)),
+    )
