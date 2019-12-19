21490f58ffb87059d81d7463d4aae4ee5859c047
kooo0401 <56294747+kooo0401@users.noreply.github.com>
2019-12-19 20:04:21 +0900

    gunicorn_setting

--- app/jointparty/wsgi.py
+++ app/jointparty/wsgi.py
@@ -14,19 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jointparty.settings.production')
 
 application = get_wsgi_application()
-
-# 20191218”jointpartyモジュールが見つからない”エラー対応
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-# from dj_static import Cling
-
-# import sys
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
-
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projectname.settings")
-
-
-# application = Cling(get_wsgi_application())
