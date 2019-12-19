0e8783a863641f4147d92a2ed2fd76257325bfc3
kooo0401 <56294747+kooo0401@users.noreply.github.com>
2019-12-19 22:54:55 +0900

    kubectl_setting_2

--- app/jointparty/settings/production.py
+++ app/jointparty/settings/production.py
@@ -5,12 +5,12 @@ import environ
 BASE_DIR = environ.Path(__file__) - 3
 SECRET_KEY = os.environ.get("SECRET_KEY")
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS") #.split(" ")
-DEBUG = 0
+DEBUG = int(os.environ.get("DEBUG"))
 
 env = environ.Env()
 
 # 環境変数でDJANGO_READ_ENV_FILEをTrueにしておくと.envを読んでくれる。
-READ_ENV_FILE = env.bool('DJANGO_READ_ENV_FILE', default=False)
+READ_ENV_FILE = env.bool('DJANGO_READ_ENV_FILE', default=True)
 if READ_ENV_FILE:
     env_file = str(BASE_DIR.path('.env.prod'))
     env.read_env(env_file)
