1d322804bca7cc7d762ba780ba3f10fe41777dab
kooo0401 <56294747+kooo0401@users.noreply.github.com>
2019-12-19 21:29:32 +0900

    gunicorn_setting_8

--- app/jointparty/settings/production.py
+++ app/jointparty/settings/production.py
@@ -3,6 +3,9 @@ import os
 import environ
 
 BASE_DIR = environ.Path(__file__) - 3
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+DEBUG = int(os.environ.get("DEBUG"))
 
 env = environ.Env()
 
@@ -12,9 +15,6 @@ if READ_ENV_FILE:
     env_file = str(BASE_DIR.path('.env.prod'))
     env.read_env(env_file)
  
-DEBUG = int(os.environ.get("DEBUG"))
-
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # 一旦コメントアウト、後ほど20191219
 # DATABASES = {
