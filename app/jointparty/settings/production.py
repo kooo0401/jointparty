053f658684c4d6d8cc6b725ac7cfd88eee5d2ee0
kooo0401 <56294747+kooo0401@users.noreply.github.com>
2019-12-20 21:53:14 +0900

    db_create2

--- app/jointparty/settings/production.py
+++ app/jointparty/settings/production.py
@@ -16,28 +16,28 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 DEBUG = int(os.environ.get("DEBUG"))
 
-GKE_DEPLOY = env.bool('GKE_DEPLOY', default=False)
-if GKE_DEPLOY:
-    DATABASES = {
-        'default': {
-            'ENGINE': os.environ.get('DATABASE_ENGINE','django.db.backends.postgresql'),
-            'NAME': os.environ.get('DATABASE_DB', os.path.join(BASE_DIR, 'db.postgresql')),
-            'USER': os.environ.get('DATABASE_USER', 'user'),
-            'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'password'),
-            'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
-            'PORT': os.environ.get('DATABASE_PORT', '5432'),
-        }
+# GKE_DEPLOY = env.bool('GKE_DEPLOY', default=False)
+# if GKE_DEPLOY:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': os.environ.get('DATABASE_ENGINE','django.db.backends.postgresql'),
+#             'NAME': os.environ.get('DATABASE_DB', os.path.join(BASE_DIR, 'db.postgresql')),
+#             'USER': os.environ.get('DATABASE_USER', 'user'),
+#             'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'password'),
+#             'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
+#             'PORT': os.environ.get('DATABASE_PORT', '5432'),
+#         }
+#     }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': '/cloudsql/jointparty-sql',
+        'NAME': 'polls',
+        'USER': 'admin',
+        'PASSWORD': 'horikoudai',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'HOST': '/cloudsql/jointparty-sql',
-            'NAME': 'polls',
-            'USER': 'admin',
-            'PASSWORD': 'horikoudai',
-        }
-    }
+}
 
 
 WSGI_APPLICATION = 'jointparty.wsgi.application'
