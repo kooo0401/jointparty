7f95931df98f58a1a261888b6556afaf7471b40d
kooo0401 <56294747+kooo0401@users.noreply.github.com>
2020-01-17 12:44:03 +0900

    userinfo_edit_apply

--- app/match/forms/user_edit.py
+++ app/match/forms/user_edit.py
@@ -1,39 +1,10 @@
 from django import forms
-from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
-from match.models.image import Image
+from django.db import models
 from match.models.userinfo import UserInfo
 
-class EditForm(forms.Form):
-    image = forms.ImageField()
-    error_message = ''
-    username = forms.CharField(label='LOGIN_ID', max_length=20, widget=forms.TextInput(attrs={'placeholder':'名前を入力して下さい', 'class':'form-control'}))
-    password = forms.CharField(label='PASSWORD', max_length=20, widget=forms.PasswordInput(attrs={'placeholder':'パスワードを入力して下さい', 'class':'form-control', 'autocomplete':'off'}))
-    SEX = [('man','男'),('woman','女')]
-    sex = forms.ChoiceField(choices=SEX, widget=forms.RadioSelect())
-    self_introduction = forms.CharField(required=False, label='SELF_INTRODUCTION', max_length=200, widget=forms.Textarea(attrs={'rows':10, 'class':'form-control'}))
-    info = UserInfo()
-    is_save = False
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class UserUpdateForm(forms.ModelForm):
 
-    def load(self, userid):
-        self.info = UserInfo.objects.get(pk=userid)
-
-    def save(self, post, userid):
-        user = User.objects.get(pk=userid)
-        user.set_password(post['password'])
-        user.save()
-
-        image = Image.objects.get(pk=userid)
-        image.origin = self.cleaned_data['image']
-        image.save()
-
-        info = UserInfo.objects.get(pk=userid)
-        info.name = post['username']
-        info.self_introduction = post['self_introduction']
-        info.sex = post['sex']
-        info.save()
-
-        self.is_save = True
+    class Meta:
+        model = UserInfo
+        fields = ('name', 'self_introduction',)
--- app/match/views/user.py
+++ app/match/views/user.py
@@ -2,9 +2,16 @@ from django.shortcuts import render, loader, redirect
 from django.http import HttpResponse
 from match.forms.user_signup import SignupForm
 from match.forms.user_profile import ProfileForm
-from match.forms.user_edit import EditForm
 from match.forms.user_list import UserListForm
 from django.contrib.auth.decorators import login_required
+from django.views.generic import FormView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from match.models.userinfo import UserInfo
+from match.models.image import Image
+from django.urls import reverse_lazy
+from match.forms.user_edit import UserUpdateForm
+from django.urls import reverse
+from django import forms
 
 
 def signup(request):
@@ -37,33 +44,43 @@ def gets(request):
     }
     return HttpResponse(template.render(context, request))
 
-def profile(request, user_id):
+def profile(request, pk):
     if request.method == 'GET':
         form = ProfileForm(request.GET or None)
-        form.load(user_id)
+        form.load(pk)
         data = {
             'form': form,
-            'iscurrent': request.user.id == user_id,
+            'iscurrent': request.user.id == pk,
         }
         return render(request, 'match/profile.html', data)
 
-@login_required
-def edit(request, user_id):
-    if request.method == 'GET':
-        form = EditForm(request.GET or None)
-        form.nad(user_id)
-    else:
-        form = EditForm(request.POST or None, request.FILES)
-        if form.is_valid():
-            print('user_edit is_valid')
-            form.save(request.POST, user_id)
-        else:
-            print('user_edit false is_valid')
-        if form.is_save:
-            return redirect('/users/profile/' + str(user_id))
+# カレントユーザー以外は404エラーを発生させる
+class OnlyYouMixin(UserPassesTestMixin):
+    raise_exception = True
 
-    template = loader.get_template('match/edit.html')
-    context = {
-        'form': form,
-    }
-    return HttpResponse(template.render(context, request))
+    def test_func(self):
+        user = self.request.user
+        return user.pk == self.kwargs['pk'] or user.is_superuser
+
+class UserUpdateView(OnlyYouMixin, LoginRequiredMixin, UpdateView):
+    model = UserInfo
+    form_class = UserUpdateForm
+
+    def get_success_url(self):
+        return reverse('detail', kwargs={'pk': self.object.pk})
+
+    def get_form(self):
+        form = super(UserUpdateView, self).get_form()
+        form.fields['name'].widget = forms.TextInput(attrs = {'class': 'form-control'})
+        form.fields['name'].label = '名前'
+        form.fields['self_introduction'].widget = forms.Textarea(attrs = {'class': 'form-control'})
+        form.fields['self_introduction'].label = '自己紹介'
+        # SEX = [('man','男'), ('woman','女')]
+        # form.fields['sex'].choices = SEX
+        # form.fields['sex'].widget = forms.RadioSelect()
+        # form.fields['sex'].label = '性別'
+        return form
+
+    # 動的URLにリダイレクトする際は、下記メソッドを使う
+    def get_success_url(self):
+        return reverse_lazy('match:profile', kwargs={"pk":self.kwargs["pk"]})
